c444856cf24cd211b4fa618779b83662171079e4
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-06-15 10:57:44 +0800

    Add example 0.

---
+++ Example 0/ProjectSimpleCore/ProjectSimpleCore.cpp
@@ -0,0 +1,28 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+extern "C" void Test()
+{
+    ::MessageBoxW(
+        nullptr,
+        L"Hello World!",
+        L"ProjectSimpleCore.dll",
+        0);
+}
+
+BOOL APIENTRY DllMain(
+    HMODULE hModule,
+    DWORD  ul_reason_for_call,
+    LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
