40ce9a32a997946bcec66a2e2a1bedb5183af5f2
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-06-15 11:30:46 +0800

    Add example 3.

---
+++ Example 3/ImageBuilder/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Dism;
+
+namespace ImageBuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DismApi.Initialize(DismLogLevel.LogErrors);
+            }
+            finally
+            {
+                DismApi.Shutdown();
+            }
+        }
+    }
+}
